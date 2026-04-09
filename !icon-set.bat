965b1adb750bab9b68f277ac78e9029ede36c29b
hummer74 <hummer74rus@gmail.com>
2026-04-10 00:57:20 +0500

    modified:   !icon-set.bat

--- !icon-set.bat
+++ !icon-set.bat
@@ -4,15 +4,3 @@ cd /d "%~dp0"
 go install github.com/akavel/rsrc@latest
 rsrc -ico GoProxy.ico
 go build -ldflags="-s -w -linkmode=external -extldflags=-static" -o GoProxy.exe .
-
-@echo off
-if %ERRORLEVEL% NEQ 0 (
-    echo [ERROR] Build failed, skipping UPX.
-    pause
-    exit /b %ERRORLEVEL%
-)
-timeout /t 5 /nobreak >nul
-exit 0
-upx --ultra-brute GoProxy.exe
-timeout /t 1 /nobreak >nul
-exit 0
